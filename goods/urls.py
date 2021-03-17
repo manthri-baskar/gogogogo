9dbd462b2c67ba06c7f39b89f03050a48fcd08ea
manthri-baskar <mrbroyal.9701@gmail.com>
2021-03-17 12:29:13 +0530

    add raw material to existing good

--- goods/urls.py
+++ goods/urls.py
@@ -3,8 +3,9 @@ from .views import *
 
 app_name = 'goods' 
 
-urlpatterns = [
+urlpatterns = [ 
     path('', goods_form_view, name='goods_form_url'),
     path('amount/', amount_form_view, name='amount_form_url'),
     path('delete_items/', delete_goods, name="delete_goods_url"),
+    path('raw_to_good/', add_rawTo_good, name="add_rawTo_good_url"),
 ] 
